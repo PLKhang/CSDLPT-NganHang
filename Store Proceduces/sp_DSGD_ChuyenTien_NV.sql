5b0aefca4feb73c6ef2cb439eff11bab3f7e4a5d
Lê Phúc Khang <plkhang090403@gmail.com>
2024-04-19 12:05:10 +0700

    Update sp_DSGD_ChuyenTien_NV.sql

--- Store Proceduces/sp_DSGD_ChuyenTien_NV.sql
+++ Store Proceduces/sp_DSGD_ChuyenTien_NV.sql
@@ -2,5 +2,5 @@ create proc sp_DSGD_ChuyenTien_NV
 @MANV nchar(10)
 as
 select MAGD, NGAYGD, SOTK_CHUYEN, SOTK_NHAN, SOTIEN
-from LINK0.NGANHANG.dbo.GD_CHUYENTIEN as GD
+from dbo.GD_CHUYENTIEN
 where MANV = @MANV
