b4d91c68ee168e5af2f78792be12d997e75605b9
PLKhang <plkhang090403@gmail.com>
2024-04-22 00:38:00 +0700

    chỉ kiểm tra side hiện tại khi thêm NV

--- Store Proceduces/sp_Existed_CMND_NV.sql
+++ Store Proceduces/sp_Existed_CMND_NV.sql
@@ -1,7 +1,9 @@
 create proc sp_Existed_CMND_NV
 @CMND nchar(10)
 as
-IF exists (select CMND from LINK0.NGANHANG.dbo.NHANVIEN where CMND = @CMND)
-	return 1;
-ELSE
-	return 0;
+BEGIN
+	IF exists (select CMND from dbo.NHANVIEN where CMND = @CMND)
+		return 1;
+	ELSE
+		return 0;
+END
