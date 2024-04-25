e531215994f627091371b74c47827e065ea1c878
ThienTanMk <125727960+ThienTanMk@users.noreply.github.com>
2024-04-26 00:12:19 +0700

    Update and rename sp_GuiTien.sql to sp_GuiRut.sql

---
+++ Store Proceduces/sp_GuiRut.sql
@@ -0,0 +1,52 @@
+ALTER PROC [dbo].[SP_GuiRut] (@SOTK	NCHAR(9), @TIEN MONEY, @LOAIGD NCHAR(2),@MANV NCHAR(10))
+AS
+SET XACT_ABORT ON;
+SET TRANSACTION ISOLATION LEVEL SERIALIZABLE;
+BEGIN TRANSACTION		-- Nếu chỉ SP chỉ có một lệnh UPDATE thì ta không cần sử dụng giao tác vì nó là giao tác tự động.
+	BEGIN TRY			-- Nhưng SP này ta có 2 lệnh là UPDATE và INSERT.
+		IF EXISTS(SELECT SOTK FROM dbo.TaiKhoan WHERE SOTK = @SOTK)	-- Kiểm tra stk có tồn tại hay không, sau đó xét xem nó là 'RT' hay 'GT'
+			BEGIN
+				DECLARE @SODUDAU MONEY;
+			  SELECT @SODUDAU = SODU FROM dbo.TaiKhoan WHERE SOTK = @SOTK;
+			  DECLARE @SODUSAU MONEY;
+
+				IF @LOAIGD = 'GT'
+					BEGIN
+						UPDATE dbo.TaiKhoan
+						SET @SODUSAU = @SODUDAU + @TIEN
+						WHERE SOTK = @SOTK
+
+						INSERT INTO GD_GOIRUT(SOTK,LOAIGD,NGAYGD,SOTIEN,MANV) 
+							VALUES(@SOTK,@LOAIGD,GETDATE(),@TIEN,@MANV)
+					END
+
+	
+				ELSE IF @LOAIGD = 'RT'
+					BEGIN
+						IF @SODUDAU < @TIEN
+							RAISERROR('SO DU KHONG DU !!!', 16, 1)
+						ELSE
+							BEGIN
+								UPDATE dbo.TaiKhoan
+								SET @SODUSAU = @SODUDAU - @TIEN
+								WHERE S0TK = @SOTK
+
+								INSERT INTO GD_GOIRUT(SOTK,LOAIGD,NGAYGD,SOTIEN,MANV) 
+									VALUES(@SOTK,@LOAIGD,GETDATE(),@TIEN,@MANV)
+							END
+					END
+			END
+		ELSE
+			RAISERROR('SO TAI KHOAN KHONG TON TAI !!!', 16, 1)
+		COMMIT TRANSACTION;
+	END TRY
+	
+	BEGIN CATCH
+		IF (@@TRANCOUNT > 0)
+		BEGIN
+			ROLLBACK TRANSACTION;
+			DECLARE @ErrorMessage NVARCHAR(2000)
+			SELECT @ErrorMessage = 'LOI: ' + ERROR_MESSAGE()
+			RAISERROR (@ErrorMessage,16,1)
+		END
+	END CATCH
