f23ad79f3aa78bafe769497714a4464f792344bd
PLKhang <plkhang090403@gmail.com>
2024-06-19 23:06:02 +0700

    Search on current site

--- Store Proceduces/sp_DanhSachTaiKhoan_ChiNhanh.sql
+++ Store Proceduces/sp_DanhSachTaiKhoan_ChiNhanh.sql
@@ -4,7 +4,7 @@ BEGIN
       IF (@Loai = 'T')--TẤT CẢ CHI NHÁNH
       BEGIN
             SELECT TK.SOTK,KH.HO +' ' + KH.TEN AS HOTEN,TK.CMND,TK.MACN,TK.NGAYMOTK
-            FROM LINK2.NGANHANG.dbo.KhachHang AS KH,
+            FROM dbo.KhachHang AS KH,
                   (     SELECT SOTK,CMND,MACN,NGAYMOTK 
                         FROM TaiKhoan -- with (index = IX_TAIKHOAN_NGAYMO)
                         WHERE NGAYMOTK BETWEEN @dateFrom AND @dateTo) AS TK 
@@ -14,7 +14,7 @@ BEGIN
       ELSE IF (@Loai = 'C')--CHI NHÁNH HIỆN TẠI
       BEGIN
             SELECT TK.SOTK,KH.HO +' ' + KH.TEN AS HOTEN,TK.CMND,TK.MACN,TK.NGAYMOTK
-            FROM LINK2.NGANHANG.dbo.KhachHang AS KH,
+            FROM NGANHANG.dbo.KhachHang AS KH,
                   (     SELECT SOTK,CMND,MACN,NGAYMOTK 
                         FROM TaiKhoan -- with (index = IX_TAIKHOAN_NGAYMO)
                         WHERE NGAYMOTK BETWEEN @dateFrom AND @dateTo AND MACN = @MACN) AS TK 
--- Store Proceduces/sp_TaoTK_KH.sql
+++ Store Proceduces/sp_TaoTK_KH.sql
@@ -1,43 +1,40 @@
 -- t đang lầm giữa tạo tk login cho khách hàng với tạo tk, hình như cái này t viết là tạo 1 Tk trong db
 CREATE PROCEDURE [dbo].[sp_TaoTK_KH] 
-  @SOTK nchar(9), 
-  @CMND nchar(10), 
-  @SODU money, 
-  @MACN nchar(10), 
-  @NGAYMOTK datetime
+@SOTK nchar(9), 
+@CMND nchar(10), 
+@SODU money, 
+@MACN nchar(10), 
+@NGAYMOTK datetime
 AS
 BEGIN
 	SET XACT_ABORT ON
 	BEGIN TRY
 		BEGIN TRANSACTION
 		-- Kiem tra su ton tai cua khach hang bang CMND
-    declare @existedCMND int
-	  exec @existedCMND = sp_Existed_CMND_KH @CMND
-	  if(@existedCMND = 1)
-	  BEGIN
-		  rollback
-	  	RETURN 1 -- Thêm không thành công
-	  ENd
-  -- CMND ko tồn tại
-  	if(@existedCMND = 0)
-		  BEGIN
-		  	INSERT INTO TaiKhoan (SOTK , CMND , SODU , MACN , NGAYMOTK)
-					VALUES (@SOTK , @CMND , @SODU , @MACN ,@NGAYMOTK)
-	  	END 
-  -- CMND tồn tại
-    if(@existedCMND = 1)
-	  	BEGIN
-		    rollback
-		    RETURN 1 -- Thêm không thành công
-	  	END 
-		COMMIT TRANSACTION
+		declare @existedCMND int
+		exec @existedCMND = sp_Existed_CMND_KH @CMND
+		if(@existedCMND = 0)
+			BEGIN
+				RAISERROR(N'Khách hàng không tồn tại', 16, 1);
+				ROLLBACK; -- Rollback nếu khách hàng không tồn tại
+				RETURN;
+			END
+		-- CMND ko tồn tại
+		ELSE
+			BEGIN
+				INSERT INTO TaiKhoan (SOTK , CMND , SODU , MACN , NGAYMOTK)
+				VALUES (@SOTK , @CMND , @SODU , @MACN ,@NGAYMOTK)
+				COMMIT TRANSACTION
+			END 
 	END TRY
+
 	BEGIN CATCH
-    ROLLBACK TRANSACTION
-		DECLARE @ErrorMessage VARCHAR(MAX), @ErrorSeverity INT, @ErrorState INT
-		SET @ErrorMessage  = ERROR_MESSAGE()
-		SET @ErrorSeverity = ERROR_SEVERITY()
-		SET @ErrorState    = ERROR_STATE()
-		RAISERROR(@ErrorMessage, @ErrorSeverity, @ErrorState)
-  END CATCH
+		IF (@@TRANCOUNT > 0)
+			BEGIN
+				ROLLBACK TRANSACTION;
+				DECLARE @ErrorMessage NVARCHAR(2000)
+				SELECT @ErrorMessage = 'LOI: ' + ERROR_MESSAGE()
+				RAISERROR (@ErrorMessage,16,1)
+			END
+	END CATCH
 END
