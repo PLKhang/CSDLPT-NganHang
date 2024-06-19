7471da5ef810326bc743bb5906181a417e8891fe
PLKhang <plkhang090403@gmail.com>
2024-06-19 22:56:54 +0700

    Search on current site

--- Store Proceduces/sp_Existed_STK.sql
+++ Store Proceduces/sp_Existed_STK.sql
@@ -2,7 +2,7 @@ create proc sp_Existed_STK
 @SOTK nchar(9)
 as
 BEGIN
-	IF exists (select SOTK from LINK2.NGANHANG.dbo.TAIKHOAN where SOTK = @SOTK)
+	IF exists (select SOTK from dbo.TAIKHOAN where SOTK = @SOTK)
 		return 1;
 	ELSE
 		return 0;
